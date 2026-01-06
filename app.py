48985c714eaead3e75e89881f103773b3556b722
Sroczysk0 <magsroka04@gmail.com>
2026-01-06 18:44:05 +0100

    Dodane dockercompose z dwoma kontenerami

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
+import redis
+import os
 
 app = Flask(__name__)
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
 
 @app.route("/")
 def home():
@@ -14,5 +19,11 @@ def health():
 def items():
     return jsonify(["apple", "banana", "orange"])
 
+@app.route("/counter")
+def counter():
+    count = r.incr("visits")
+    return jsonify(visits=count)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
